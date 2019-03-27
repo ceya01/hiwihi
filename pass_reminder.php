e78740889f839396ae255b20a89b456ba97b2ba6
ceya <regist.ceya@gmail.com>
2019-03-27 12:41:03 +0900

    パスワードリマインダーページ基礎デザイン

--- pass_reminder.php
+++ pass_reminder.php
@@ -3,13 +3,19 @@
 <?php require_once( "include/header.php" ); ?>
 
 <!--  メイン -->
-<main>
-    <h2>パスワードリマインダー</h2>
-    <p>パスワードをリセットして、入力したメールアドレスに新パスワードを送信します。</p>
-    <form action="" method="post">
-        <label>メールアドレス<input type="text"></label>
-        <input type="submit" value="メール送信">
-    </form>
+<main class="bg-hiwihiBird">
+    <div class="bg-overlay">
+        <div class="inner formWrap">
+            <h2  class="color-hiwihi mb2rem">パスワードリマインダー</h2>
+            <p class="mb1rem">パスワードをリセットして、入力したメールアドレスに新パスワードを送信します。</p>
+            <form action="" method="post">
+                <label class="inputWrap"><span class="label-text">メールアドレス</span>
+                    <input type="text" name="<?php echo KEY_EMAIL ?>" value="<?php echoPost(KEY_EMAIL); ?>">
+                <input type="submit" value="メール送信" class="btn-rr bgColor-hiwihi mt1rem">
+                </label>
+            </form>
+        </div>
+    </div>
 </main>
 
 
