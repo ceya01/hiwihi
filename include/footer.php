43c823af0d70b1669cf527783d5c220b20a2af8b
ceya <regist.ceya@gmail.com>
2019-04-12 17:28:29 +0900

    サブディレクトリに.htaccessにて簡易アクセス制限

--- include/footer.php
+++ include/footer.php
@@ -36,62 +36,11 @@
 </footer>
 
 <?php
-if (IS_DEBUG) :
-    //footerの下にデバッグ用リンクを表示する
+//footerの下にデバッグ用リンクを表示する
+if (IS_DEBUG) {
+    include(dirname(__FILE__).'/footer_debug.php');
+}
 ?>
-<div class="debugArea inner mt2rem">Debug Tool!
-    <div style="display: flex; margin-bottom: 30px;">
-        <div style="padding-left: 30px;     max-width: 40%;">
-            <ul>
-                <li><?php echoHeaderLink(PAGE_ABOUT, 'ヒウィッヒヒーとは'); ?></li>
-                <li><?php echoHeaderLink(PAGE_TIMELINE, 'タイムライン'); ?></li>
-                <li><?php echoHeaderLink(PAGE_USER, 'ユーザーページ'); ?></li>
-                <li><?php echoHeaderLink(PAGE_TWEET, 'ツイート'); ?></li>
-                <li><?php echoHeaderLink(PAGE_LOGIN, 'ログイン'); ?></li>
-                <li><?php echoHeaderLink(PAGE_SIGNUP, '新規登録'); ?></li>
-                <li><?php echoHeaderLink(PAGE_PASS_REMINDER, 'パスワードリマインダー'); ?></li>
-            </ul>
-            <?php
-            echo('<br>time:'.time().'<br>');
-            echo('<br>$_POST<br>');
-            dump($_POST);
-            echo('<br>$_SESSION<br>');
-            echo('session_id(): '.session_id());
-            echo '<br>';
-            dump($_SESSION);
-            echo('<br>$_COOKIE<br>');
-            dump($_COOKIE);
-            echo('<br>$_SERVER<br>');
-            dump($_SERVER);
-            ?>
-        </div>
-        <div style="padding-left: 30px;">
-            <p>ユーザー切り替え</p>
-            <ul>
-            <?php require_once(dirname(__FILE__).'/../core/db/table/UserTable.php');
-                $userIDs = UserTable::getAllUserList(100,'id,name,char_id,email');
-                foreach ($userIDs as $item)
-                {
-                    $uID = $item['id'];
-                    $uName = $item['name'];
-                    $uCID = $item['char_id'];
-                    $uEmail = $item['email'];
-                    $linkText = "ID: $uID $uName @$uCID $uEmail";
-                    ?>
-                    <li>
-                        <a href="debugLogin.php?uid=<?php echo $uID ?>">
-                            <?php echo $linkText; ?></a>
-                    </li>
-                    <?php
-                }
-
-             ?>
-            </ul>
-        </div>
-    </div>
-
-</div>
-<?php endif; ?>
 
 </body>
 </html>
---
+++ include/footer_debug.php
@@ -0,0 +1,53 @@
+
+<div class="debugArea inner mt2rem">Debug Tool!
+    <div style="display: flex; margin-bottom: 30px;">
+        <div style="padding-left: 30px;     max-width: 40%;">
+            <ul>
+                <li><?php echoHeaderLink(PAGE_ABOUT, 'ヒウィッヒヒーとは'); ?></li>
+                <li><?php echoHeaderLink(PAGE_TIMELINE, 'タイムライン'); ?></li>
+                <li><?php echoHeaderLink(PAGE_USER, 'ユーザーページ'); ?></li>
+                <li><?php echoHeaderLink(PAGE_TWEET, 'ツイート'); ?></li>
+                <li><?php echoHeaderLink(PAGE_LOGIN, 'ログイン'); ?></li>
+                <li><?php echoHeaderLink(PAGE_SIGNUP, '新規登録'); ?></li>
+                <li><?php echoHeaderLink(PAGE_PASS_REMINDER, 'パスワードリマインダー'); ?></li>
+            </ul>
+            <?php
+            echo('<br>time:'.time().'<br>');
+            echo('<br>$_POST<br>');
+            dump($_POST);
+            echo('<br>$_SESSION<br>');
+            echo('session_id(): '.session_id());
+            echo '<br>';
+            dump($_SESSION);
+            echo('<br>$_COOKIE<br>');
+            dump($_COOKIE);
+            echo('<br>$_SERVER<br>');
+            dump($_SERVER);
+            ?>
+        </div>
+        <div style="padding-left: 30px;">
+            <p>ユーザー切り替え</p>
+            <ul>
+                <?php require_once(dirname(__FILE__).'/../core/db/table/UserTable.php');
+                $userIDs = UserTable::getAllUserList(100,'id,name,char_id,email');
+                foreach ($userIDs as $item)
+                {
+                    $uID = $item['id'];
+                    $uName = $item['name'];
+                    $uCID = $item['char_id'];
+                    $uEmail = $item['email'];
+                    $linkText = "ID: $uID $uName @$uCID $uEmail";
+                    ?>
+                    <li>
+                        <a href="debugLogin.php?uid=<?php echo $uID ?>">
+                            <?php echo $linkText; ?></a>
+                    </li>
+                    <?php
+                }
+
+                ?>
+            </ul>
+        </div>
+    </div>
+
+</div>
