6f09d8e4b48934995f8821515af24801584aae38
ceya <regist.ceya@gmail.com>
2019-04-12 17:04:00 +0900

    モバイル対応（ヘッダーを除く）

--- debugLogin.php
+++ debugLogin.php
@@ -4,12 +4,14 @@
 require_once (dirname(__FILE__).'/core/Debug.php');
 if(!IS_DEBUG){
     header('Location:index.php');
+}else{
+    if( !isset($_GET['uid']) ){
+        header('Location:index.php');
+    }
+    require_once (dirname(__FILE__).'/core/Session.php');
+    Session::setLoginUserID($_GET['uid']);
+    header('Location:timeline.php');
 }
 
-if( !isset($_GET['uid']) ){
-    header('Location:index.php');
-}
-require_once (dirname(__FILE__).'/core/Session.php');
-Session::setLoginUserID($_GET['uid']);
-header('Location:timeline.php');
+
 
--- include/header.php
+++ include/header.php
@@ -14,6 +14,7 @@
     <link rel="stylesheet" href="css/user.css">
     <link rel="stylesheet" href="css/tweet.css">
     <link rel="stylesheet" href="css/util.css">
+    <link rel="stylesheet" href="css/mobile.css">
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
     <script
             src="https://code.jquery.com/jquery-3.3.1.min.js"
